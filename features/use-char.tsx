26c65db5605ccbab78ebe15c89965f3cbb6c0bbb
cath14 <108257596+cathleys@users.noreply.github.com>
2023-04-05 14:57:54 +0800

    tests commit on main branch (#1)

--- features/use-char.tsx
+++ features/use-char.tsx
@@ -2,14 +2,16 @@ import axios from "axios";
 import md5 from "md5";
 import { useQuery } from "react-query";
 
+const PRIVATE_KEY = "8060fc014ea4e1e0b7aba3333b027a9e77a3aa05";
+const PUBLIC_KEY = "207b830184c8de5036a41f859fbda02b";
+
 const ts = Number(Date.now());
-const hash = md5(ts + process.env.PRIVATE_KEY + process.env.PUBLIC_KEY);
+const hash = md5(ts + PRIVATE_KEY + PUBLIC_KEY);
 
 async function getChar() {
   const { data } = await axios.get(
-    `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${process.env.PUBLIC_KEY}&hash=${hash}`
+    `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`
   );
-  console.log("data results", data.data.results);
   return data;
 }
 
