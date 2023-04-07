ef6285f162882da9fb0445542a39e0747bd29de4
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-04-07 16:16:54 +0800

    Adds global styles

--- styles/global-style.ts
+++ styles/global-style.ts
@@ -1,11 +1,22 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
 import { normalize } from "styled-normalize";
 
+const FontStyles = css`
+  @font-face {
+    font-family: CCBiffBamBoom;
+    src: local("CCBiffBamBoom"),
+      url("../public/fonts/CCBiffBamBoom.ttf") format("ttf");
+  }
+`;
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
- 
- 
+  ${FontStyles}
+
   body {
-   font-family: comic-neue, Sans-Serif;
+    font-family: "Comic Neue", Sans-Serif;
+  }
+
+  h1 {
+    font-family: CCBiffBamBoom, Sans-Serif;
   }
 `;
