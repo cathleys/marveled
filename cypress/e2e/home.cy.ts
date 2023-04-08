e0c8c88d6dc46a8c07f003256ac8a9d371a137c8
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-04-08 16:49:16 +0800

    adds search container

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,6 @@
 describe("home", () => {
   it("should have header banner", () => {
     cy.visit("http://localhost:3000");
-    cy.get("main").find('[data-cy="h1"]').should("be.visible");
+    cy.get("header").find('[data-cy="h1"]').should("be.visible");
   });
 });
