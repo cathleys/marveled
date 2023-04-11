b9080ecc16f9b26d732cbeca4a2836f581034307
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-04-11 12:15:22 +0800

    Adds polka dot css style

--- features/page-container.tsx
+++ features/page-container.tsx
@@ -8,6 +8,11 @@ const Container = styled.div`
   display: flex;
   background-color: #f5f5f5;
   height: 100vh;
+  background-image: radial-gradient(rgb(145, 145, 145) 15.2%, transparent 15.2%),
+    radial-gradient(rgb(145, 145, 145) 15.2%, transparent 15.2%);
+  background-position: 0px 0px, 4px 4px;
+  background-size: 8px 8px;
+  background-color: rgb(245, 245, 245);
 `;
 const Main = styled.main`
   flex: 1;
