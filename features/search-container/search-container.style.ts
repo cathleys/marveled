f71ebd196bf442e5232edfe422d8f6672e4b728c
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-04-15 13:10:13 +0800

    Adds logo in search container

--- features/search-container/search-container.style.ts
+++ features/search-container/search-container.style.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { HeaderName } from "@features";
 
 export const Container = styled.div`
   display: flex;
@@ -8,7 +9,10 @@ export const Container = styled.div`
   background-color: #343a40;
 `;
 
-export const Title = styled.p`
+export const Title = styled(HeaderName)`
+  font-size: 1rem;
+  transform: none;
+  letter-spacing: normal;
+  text-shadow: -0.5px 0.5px white, 0.5px 0.5px yellow, 1px 1px orange;
   padding-left: 10rem;
-  color: white;
 `;
