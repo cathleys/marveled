37b9f0858c5d8b8ab2be18c1214ddb389d42d86d
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-04-17 15:13:44 +0800

    Adds hero page info container

--- features/hero-page-card/index.ts
+++ features/hero-page-card/index.ts
@@ -1,3 +1,6 @@
 export { heroPageCards } from "./component/hero-page-card-data";
 export { HeroPageCard } from "./component/hero-page-card";
-export { HeroPageCardContainer } from "./component/hero-page-card.style";
+export {
+  HeroPageCardContainer,
+  ButtonWrapper
+} from "./component/hero-page-card.style";
--- features/search-container/search-container.tsx
+++ features/search-container/search-container.tsx
@@ -1,15 +1,26 @@
 import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+import { Routes } from "@config/routes";
 import { Container, LogoLink, Title } from "./search-container.style";
 
 type SearchContainerProps = {
   href: string;
 };
+
+const Hero = styled(Link)`
+  margin-left: 1rem;
+  text-decoration: none;
+  color: white;
+`;
+
 export function SearchContainer({ href }: SearchContainerProps) {
   return (
     <Container data-cy="search-container">
       <LogoLink href={href} passHref>
         <Title>Marvelous</Title>
       </LogoLink>
+      <Hero href={Routes.hero}>Hero</Hero>
     </Container>
   );
 }
