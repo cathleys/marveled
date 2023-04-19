f4ffdab9a03766f8292e6f7d44a1b99cf3c23c57
cath14 <108257596+cathleys@users.noreply.github.com>
2023-04-19 15:34:36 +0800

    hero-page-comic-show-card (#16)

--- features/index.ts
+++ features/index.ts
@@ -2,4 +2,5 @@ export * from "./page-container";
 export * from "./hero-banner";
 export * from "./hero-page-card";
 export * from "./search-container";
+export * from "./ui/hero/hero-page.style";
 export * from "./ui/button";
