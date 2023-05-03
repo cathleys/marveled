68ea4afc87726a630fcb002082a4514a8af15037
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-05-03 14:10:05 +0800

    Fixes back to top button component

--- features/ui/button/back-to-top/back-to-top-button.tsx
+++ features/ui/button/back-to-top/back-to-top-button.tsx
@@ -2,24 +2,31 @@ import React from "react";
 import styled from "styled-components";
 
 const BackToTop = styled.button`
+  background: url(/icons/back-to-top.svg) no-repeat center;
+  background-size: 20px;
+  opacity: 80%;
   position: fixed;
   bottom: 1.25rem;
   right: 1.25rem;
   background-color: #343a40;
   color: white;
-  padding: 0.625rem 1.25rem;
+  padding: 1.5rem;
   border: none;
-  border-radius: 0.625rem;
+  border-radius: 50%;
   cursor: pointer;
 
   &:hover {
     background-color: #4d455d;
+    border: 1px solid #222;
   }
 `;
 
 export function BackToTopButton() {
-  const handleBackToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
-  return <BackToTop onClick={handleBackToTop}>Back to Top</BackToTop>;
+  return (
+    <BackToTop
+      onClick={() => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }}
+    />
+  );
 }
