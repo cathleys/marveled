e4140c0142c40d4d4551229d35b7574cfca8f5b1
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-05-06 16:20:59 +0800

    refactors home page cards

--- features/home/component/home-card/home-card.style.ts
+++ features/home/component/home-card/home-card.style.ts
@@ -7,16 +7,17 @@ export const HeroPageCardContainer = styled.div`
   justify-content: space-around;
   align-items: flex-start;
 `;
-export const Card = styled.div`
+export const Card = styled.div<{ isDark: boolean }>`
   display: flex;
   flex-wrap: wrap;
   flex-direction: column;
   margin-top: 2.875rem;
   width: 14.688rem;
-  background-color: white;
+  background: ${({ isDark }) => (isDark ? "#222" : "white")};
   border-radius: 0.938rem;
   box-shadow: 0px 12px 25px rgba(0, 0, 0, 0.2);
   text-align: center;
+  color: ${({ isDark }) => (isDark ? "#F1F1F1" : "#332c39")};
 `;
 export const Image = styled.img`
   width: 14.688rem;
